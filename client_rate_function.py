4f505d80de9f2fcd7eb9f81235a63e3a2897aa33
Ugrasen Banchhor <ugrasen.banchhor@gmail.com>
2025-05-07 11:47:05 +0530

    Update client_rate_function.py

--- client_rate_function.py
+++ client_rate_function.py
@@ -4,3 +4,5 @@ import numpy as np
 class ClientRateFunction:
 
     def __init__(self, simcluster, parameter_file):
+        self.simcluster = simcluster
+        self.parameter_file = parameter_file
